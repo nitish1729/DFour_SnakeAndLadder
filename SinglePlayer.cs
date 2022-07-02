23fbc2f3fd8cf7bf74bdfc36616e0be0869f615e
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-02 16:24:31 +0530

    Snake&Ladder_SinglePlayer

---
+++ SinglePlayer.cs
@@ -0,0 +1,15 @@
+﻿
+// Snake and Ladder game played with single player at start position 0	
+
+string Snake_Ladder;
+Snake_Ladder = "Welcome to the Snake & Ladder Simulator";
+Console.WriteLine(Snake_Ladder);
+Console.WriteLine("------------------------------------");
+
+Console.WriteLine("Enter the player name");
+// Console.ReadLine() to get user input
+string player = Console.ReadLine();
+
+const int start = 0; // Constant- start position 
+int position = start;
+Console.WriteLine("Play name is : {0}", player + "  " +player + "is on: " + position + " - " + "Position");
